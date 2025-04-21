32189293ff5c35ae1237c9c82bb667eb36a25571
Kiran Gundeti <51983300+kirangun@users.noreply.github.com>
2025-04-21 22:15:29 +0530

    Revert "updated return type of function call"

--- src/main/java/com/chtrembl/petstoreapp/service/ItemReserveServiceImpl.java
+++ src/main/java/com/chtrembl/petstoreapp/service/ItemReserveServiceImpl.java
@@ -25,12 +25,14 @@ public class ItemReserveServiceImpl implements ItemReserveService {
 
     private static final Logger logger = LoggerFactory.getLogger(ItemReserveServiceImpl.class);
 
+    private final User sessionUser;
     private final ContainerEnvironment containerEnvironment;
     private final WebRequest webRequest;
 
     private WebClient itemReserverWebClient = null;
 
-    public ItemReserveServiceImpl(ContainerEnvironment containerEnvironment, WebRequest webRequest) {
+    public ItemReserveServiceImpl(User sessionUser, ContainerEnvironment containerEnvironment, WebRequest webRequest) {
+        this.sessionUser = sessionUser;
         this.containerEnvironment = containerEnvironment;
         this.webRequest = webRequest;
     }
@@ -49,14 +51,13 @@ public class ItemReserveServiceImpl implements ItemReserveService {
         logger.info("order json before sending to function {}", orderJSON);
 
         Consumer<HttpHeaders> consumer = it -> it.addAll(this.webRequest.getHeaders());
-        this.itemReserverWebClient.post().uri("api/processOrder")
+         this.itemReserverWebClient.post().uri("api/processOrder")
                 .body(BodyInserters.fromPublisher(Mono.just(orderJSON), String.class))
                 .accept(MediaType.APPLICATION_JSON)
                 .headers(consumer)
                 .header("Content-Type", MediaType.APPLICATION_JSON_VALUE)
                 .header("Cache-Control", "no-cache")
                 .retrieve()
-                .toBodilessEntity()
-                .block();
+                .bodyToMono(Order.class).block();
     }
 }
